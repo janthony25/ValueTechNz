6e6311714577b51378928d1275e7c07d6cbe8bf5
Jun Anthony <junanthony.business@gmail.com>
2024-11-06 21:07:02 +1300

    Fix Layout Navbar and Logout button

--- ValueTechNz/Controllers/AccountController.cs
+++ ValueTechNz/Controllers/AccountController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using NPOI.OpenXmlFormats.Dml.Chart;
+using ValueTechNz.Models;
 using ValueTechNz.Models.Dto;
 using ValueTechNz.Repository.IRepository;
 
@@ -8,12 +10,14 @@ namespace ValueTechNz.Controllers
     public class AccountController : Controller
     {
         private readonly IAccountRepository _accountRepository;
+        private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly ILogger<AccountController> _logger;
 
-        public AccountController(IAccountRepository accountRepository, ILogger<AccountController> logger)
+        public AccountController(IAccountRepository accountRepository, ILogger<AccountController> logger, SignInManager<ApplicationUser> signInManager)
         {
             _accountRepository = accountRepository;
             _logger = logger;
+            _signInManager = signInManager;
         }
 
         public async Task<IActionResult> Register()
@@ -34,9 +38,11 @@ namespace ValueTechNz.Controllers
                     {
                         foreach(var error in modelStateEntry.Errors)
                         {
+
                             _logger.LogError($"Validation error: {error.ErrorMessage}");
                         }
                     }
+                    return View(registerDto);
                 }
 
 
@@ -49,6 +55,25 @@ namespace ValueTechNz.Controllers
                 _logger.LogError(ex, "An error occurred while trying to process user registration.");
                 TempData["ErrorMessage"] = "An error occurred while trying to process your registration.";
                 return RedirectToAction("Register");
+            }
+        }
+
+        // GET: Logout User
+        public async Task<IActionResult> Logout()
+        {
+            try
+            {
+                if (_signInManager.IsSignedIn(User))
+                {
+                    await _signInManager.SignOutAsync();
+                }
+                return RedirectToAction("Index", "Home");
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while trying to logout.");
+                TempData["ErrorMessage"] = "An error occurred while trying to logout";
+                return RedirectToAction("Index", "Home");
             }
         }
     }
